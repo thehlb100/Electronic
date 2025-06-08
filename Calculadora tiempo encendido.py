660036b09fa25bef048873bd63ed89d680504cf9
thehlb100 <156230000+thehlb100@users.noreply.github.com>
2025-06-08 09:06:38 +0200

    Delete Calculadora tiempo encendido.py

--- Calculadora tiempo encendido.py
+++
@@ -1,9 +0,0 @@
-#CALCULADORA DE TIEMPO ENCENDIDO
-print ("Cual es la resistencia total (ohm)?")
-ResistenciaTotal= int(input())
-print ("Cual es la capacidad del condensador (F)?")
-CapacidadCondensador = int(input())
-tiempo = 1,1 * ResistenciaTotal * CapacidadCondensador
-
-
-print (f'El led durara {tiempo}s encendido')

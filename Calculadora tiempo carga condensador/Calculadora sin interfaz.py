e05822651ec5888c375a21ea9d9ed22333c8e40f
thehlb100 <156230000+thehlb100@users.noreply.github.com>
2025-06-08 09:05:17 +0200

    Delete Calculadora tiempo carga condensador directory

--- Calculadora tiempo carga condensador/Calculadora sin interfaz.py
+++
@@ -1,9 +0,0 @@
-
-#CALCULADORA TIEMPO CARGA DE UN CONDENSADOR
-print ('Bienvenido a la calculadora de tiempo de carga de un condensador')
-print ('Cual es el valor de la resistencia en ohm?')
-resistencia = int(input())
-print ('Cual es el valor de la capacidad del condensador en F?')
-Capacidad_Condensador = int(input())
-Calculo = 5*resistencia*Capacidad_Condensador
-print (f'El condensador tarda {Calculo}s en cargar')

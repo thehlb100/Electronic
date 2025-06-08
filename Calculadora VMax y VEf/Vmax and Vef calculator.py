284bd326915fabbc13195c3a6e5f4dab284cb7a0
thehlb100 <156230000+thehlb100@users.noreply.github.com>
2025-06-08 09:04:52 +0200

    Delete Calculadora VMax y VEf directory

--- Calculadora VMax y VEf/Vmax and Vef calculator.py
+++
@@ -1,9 +0,0 @@
-print ("Dime el vaLor eficaz")
-pi = 3.14156
-Raiz2 = 1.414
-Vef= int (input ())
-Vmax = Vef * Raiz2
-Vmed = Vmax / pi 
-
-print ("El valor máximo es =", Vmax)
-print ("El valor medio es =", Vmed)
